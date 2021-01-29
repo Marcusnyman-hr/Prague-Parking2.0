dc964791e6e8f2df8c26ab4c4fa2f3d4763dede9
Marcus Nyman <Marcusnyman556@gmail.com>
2021-01-29 16:18:55 +0100

    2.0 typ klart?

--- PragueParking2.0/Configuration.cs
+++ PragueParking2.0/Configuration.cs
@@ -8,10 +8,24 @@ namespace PragueParking2._0
 {
     public class Configuration
     {
+        public Configuration(int parkingSpotsAmount, int parkingSpotSize, int bicycleSize, int mcSize, int carSize, int bicyclePrice, int mcPrice, int carPrice)
+        {
+            this.ParkingSpotsAmount = parkingSpotsAmount;
+            this.ParkingSpotSize = parkingSpotSize;
+            this.BicycleSize = bicycleSize;
+            this.McSize = mcSize;
+            this.CarSize = carSize;
+            this.BicyclePrice = bicyclePrice;
+            this.McPrice = mcPrice;
+            this.CarPrice = carPrice;
+        }
         public int ParkingSpotsAmount { get; set; }
         public int ParkingSpotSize { get; set; }
         public int BicycleSize{ get; set; }
         public int  McSize{ get; set; }
         public int CarSize { get; set; }
+        public int BicyclePrice { get; set; }
+        public int McPrice { get; set; }
+        public int CarPrice { get; set; }
     }
 }
--- PragueParking2.0/ParkingSpot.cs
+++ PragueParking2.0/ParkingSpot.cs
@@ -23,6 +23,102 @@ namespace PragueParking2._0
         {
             return ParkedVehiclesOnSpot;
         }
+
+        public int findIndexOfVehicle(string tokenOrRegistrationNumber)
+        {
+            int idx = 0;
+                foreach (Vehicle vehicle in ParkedVehiclesOnSpot)
+                {
+                    if (vehicle.RegistrationNumber == tokenOrRegistrationNumber || vehicle.Token == tokenOrRegistrationNumber)
+                    {
+                        return idx;
+                    }
+                    idx++;
+                }
+            return -1;
+        }
+        public bool checkForFreeSpace(int vehicleSize)
+        {
+            if(vehicleSize <= this.FreeSpace)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+        public void removeVehicle(int vehicleIndex)
+        {
+            int vehicleSize = getVehicleSize(this.ParkedVehiclesOnSpot[vehicleIndex]);
+            this.ParkedVehiclesOnSpot.RemoveAt(vehicleIndex);
+            this.FreeSpace = this.FreeSpace + vehicleSize;
+        }
+        public int getVehicleSize(Vehicle vehicle)
+        {
+            int sizeOfVehicle = -1;
+            if (vehicle.GetType().Equals(typeof(Bicycle)))
+            {
+                sizeOfVehicle = Program.config.BicycleSize;
+            }
+            if (vehicle.GetType().Equals(typeof(Mc)))
+            {
+                sizeOfVehicle = Program.config.McSize;
+            }
+            if (vehicle.GetType().Equals(typeof(Car)))
+            {
+                sizeOfVehicle = Program.config.CarSize;;
+            }
+
+            return sizeOfVehicle;
+        }
+        public int getVehiclePrice(Vehicle vehicle)
+        {
+            int pricePerHour = 0;
+            if (vehicle.GetType().Equals(typeof(Bicycle)))
+            {
+                pricePerHour = Program.config.BicyclePrice;
+            }
+            if (vehicle.GetType().Equals(typeof(Mc)))
+            {
+                pricePerHour = Program.config.McPrice;
+            }
+            if (vehicle.GetType().Equals(typeof(Car)))
+            {
+                pricePerHour = Program.config.CarPrice; ;
+            }
+            return pricePerHour;
+        }
+        public float calculateCharge(int indexToCalculate)
+        {
+            int pricePerHour = getVehiclePrice(ParkedVehiclesOnSpot[indexToCalculate]);
+            var expiredMinutes = (DateTime.Now - ParkedVehiclesOnSpot[indexToCalculate].ParkedSince).TotalMinutes;
+            if (expiredMinutes < 10)
+            {
+                return 0;
+            }
+            else
+            {
+                float totalPrice = (int)expiredMinutes * pricePerHour / 60;
+                return totalPrice;
+            }
+        }
+        
+        public float deParkVehicleFromSpot(int indexToRemove, Configuration config)
+        {
+            int sizeOfDeparkedVehicle = getVehicleSize(ParkedVehiclesOnSpot[indexToRemove]);
+            int pricePerHour = getVehiclePrice(ParkedVehiclesOnSpot[indexToRemove]);
+            var expiredMinutes = (DateTime.Now - ParkedVehiclesOnSpot[indexToRemove].ParkedSince).TotalMinutes;
+            ParkedVehiclesOnSpot.RemoveAt(indexToRemove);
+            this.FreeSpace = this.FreeSpace + sizeOfDeparkedVehicle;
+            if (expiredMinutes < 10)
+            {
+                return 0;
+            } else
+            {
+                float totalPrice = (int)expiredMinutes * pricePerHour/60;
+                return totalPrice;
+            }
+        }
         public int FreeSpace { get; set; }
         public int ParkingSpotNumber { get; set; }
         public List<Vehicle> ParkedVehiclesOnSpot = new List<Vehicle>();
